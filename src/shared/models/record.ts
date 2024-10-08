b2f40da8ec9af151347b58e2cf01980b3e5192d5
yassa-alqess <iscoadms2@gmail.com>
2024-10-08 15:06:43 +0300

    refactor: remove unnecessary indexes

--- src/shared/models/record.ts
+++ src/shared/models/record.ts
@@ -30,11 +30,9 @@ const RecordSchema: Schema = new Schema({
 
 
 // Add indexes to optimize queries
-RecordSchema.index({ url: 1 });
 RecordSchema.index({ username: 1 });
 RecordSchema.index({ created_at: 1 });
 RecordSchema.index({ status: 1 });
-RecordSchema.index({ leaked_sources: 1 });
 RecordSchema.index({ username: 1, status: 1 });
 RecordSchema.index({ created_at: 1, status: 1, username: 1 });
 RecordSchema.index({ username: 'text', url: 'text', status: 'text' });
